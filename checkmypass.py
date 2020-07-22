47599e3529bdde702e8fbed7b4176a01ec8ea8ff
Luis Mendoza Ignacio <42331587+luismendozaignacio@users.noreply.github.com>
2020-07-22 13:02:38 -0700

    file added

---
+++ checkmypass.py
@@ -0,0 +1,38 @@
+import requests
+import hashlib
+import sys
+
+#only input the first 5 SHA1 Hash
+def requests_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    #check password if it exists in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first_five_char, tail = sha1password[:5], sha1password[5:]
+    response = requests_api_data(first_five_char)
+    return get_password_leaks_count(response, tail)
+
+#requests_api_data('wq3')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times... you should change it')
+        else:
+            print(f'{password} was NOT found.')
+    return 'Done'
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
